52280812e00140fa1f20b1c70252d3f47156fe38
rizwanmalikyk <75012445+rizwanmalikyk@users.noreply.github.com>
2021-01-31 14:33:20 +0500

    Add files via upload

--- post/views.py
+++ post/views.py
@@ -1,4 +1,5 @@
 from django.db.models import Count, Q
+from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import redirect, render, get_object_or_404, redirect, reverse
 from django.views.generic import View, ListView, DetailView, CreateView, UpdateView, DeleteView
@@ -16,6 +17,21 @@ def get_author(user):
     if qs.exists():
         return qs[0]
     return None
+
+
+class SearchView(View):
+    def get(self, request, *args, **kwargs):
+        queryset = Post.objects.all()
+        query = request.GET.get('q')
+        if query:
+            queryset = queryset.filter(
+                Q(title__icontains=query) |
+                Q(overview__icontains=query)
+            ).distinct()
+        context = {
+            'queryset': queryset
+        }
+        return render(request, 'search_results.html', context)
 
 
 def search(request):
@@ -62,6 +78,27 @@ class IndexView(View):
         return redirect("home")
 
 
+class IndexView(View):
+    form = EmailSignupForm()
+
+    def get(self, request, *args, **kwargs):
+        featured = Post.objects.filter(featured=True)
+        latest = Post.objects.order_by('-timestamp')[0:3]
+        context = {
+            'object_list': featured,
+            'latest': latest,
+            'form': self.form
+        }
+        return render(request, 'index.html', context)
+
+    def post(self, request, *args, **kwargs):
+        email = request.POST.get("email")
+        new_signup = Signup()
+        new_signup.email = email
+        new_signup.save()
+        messages.info(request, "Successfully subscribed")
+        return redirect("home")
+
 def index(request):
     featured = Post.objects.filter(featured=True)
     latest = Post.objects.order_by('-timestamp')[0:3]
@@ -80,7 +117,25 @@ def index(request):
     return render(request, 'index.html', context)
 
 
-def blog(request):
+class PostListView(ListView):
+    form = EmailSignupForm()
+    model = Post
+    template_name = 'blog.html'
+    context_object_name = 'queryset'
+    paginate_by = 1
+
+    def get_context_data(self, **kwargs):
+        category_count = get_category_count()
+        most_recent = Post.objects.order_by('-timestamp')[:3]
+        context = super().get_context_data(**kwargs)
+        context['most_recent'] = most_recent
+        context['page_request_var'] = "page"
+        context['category_count'] = category_count
+        context['form'] = self.form
+        return context
+
+
+def post_list(request):
     category_count = get_category_count()
     most_recent = Post.objects.order_by('-timestamp')[:3]
     post_list = Post.objects.all()
@@ -140,8 +195,7 @@ class PostDetailView(DetailView):
                 'pk': post.pk
             }))
 
-
-def post(request, id):
+def post_detail(request, id):
     category_count = get_category_count()
     most_recent = Post.objects.order_by('-timestamp')[:3]
     post = get_object_or_404(Post, id=id)
@@ -151,7 +205,7 @@ def post(request, id):
 
     form = CommentForm(request.POST or None)
     if request.method == "POST":
-         if form.is_valid():
+        if form.is_valid():
             form.instance.user = request.user
             form.instance.post = post
             form.save()
@@ -159,13 +213,30 @@ def post(request, id):
                 'id': post.pk
             }))
     context = {
-        'form': form,
         'post': post,
         'most_recent': most_recent,
         'category_count': category_count,
         'form': form
     }
     return render(request, 'post.html', context)
+
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    form_class = PostForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Create'
+        return context
+
+    def form_valid(self, form):
+        form.instance.author = get_author(self.request.user)
+        form.save()
+        return redirect(reverse("post-detail", kwargs={
+            'pk': form.instance.pk
+        }))
 
 
 def post_create(request):
@@ -185,12 +256,32 @@ def post_create(request):
     }
     return render(request, "post_create.html", context)
 
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_create.html'
+    form_class = PostForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Update'
+        return context
+
+    def form_valid(self, form):
+        form.instance.author = get_author(self.request.user)
+        form.save()
+        return redirect(reverse("post-detail", kwargs={
+            'pk': form.instance.pk
+        }))
+
+
 def post_update(request, id):
     title = 'Update'
     post = get_object_or_404(Post, id=id)
     form = PostForm(
-        request.POST or None, 
-        request.FILES or None, 
+        request.POST or None,
+        request.FILES or None,
         instance=post)
     author = get_author(request.user)
     if request.method == "POST":
@@ -205,6 +296,14 @@ def post_update(request, id):
         'form': form
     }
     return render(request, "post_create.html", context)
+
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = '/blog'
+    template_name = 'post_confirm_delete.html'
+
 
 def post_delete(request, id):
     post = get_object_or_404(Post, id=id)
