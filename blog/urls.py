b2bac58c65abbc96fb54e002366757e57658ca0f
rizwanmalikyk <75012445+rizwanmalikyk@users.noreply.github.com>
2021-01-31 14:34:17 +0500

    Add files via upload

--- blog/urls.py
+++ blog/urls.py
@@ -3,25 +3,34 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from post.views import (
-    index, blog, post, search, IndexView, privacypolicy, blogprivacypolicy,
-    post_create, post_update, post_delete, PostDetailView,)
+    index, search, post_list, PostListView, IndexView, privacypolicy, blogprivacypolicy, PostCreateView, PostUpdateView, PostDeleteView,
+    SearchView, post_create, post_update, post_delete, PostDetailView,)
 from marketing.views import email_list_signup
 
 urlpatterns = [
     path('admin/', admin.site.urls),
    # path('', index),
     path('', IndexView.as_view(), name='home'),
-    path('blog/', blog, name='post-list'),
-    path('search/', search, name='search'),
+    #path('blog/', blog, name='post-list'),
+    path('blog/', PostListView.as_view(), name='post-list'),
+    #path('search/', search, name='search'),
+    path('search/', SearchView.as_view(), name='search'), 
+
     path('privacypolicy/', privacypolicy, name='privacypolicy'),
     path('blog/privacypolicy/', blogprivacypolicy, name='blogprivacypolicy'),
     path('email-signup/', email_list_signup, name='email-list-signup'),
-    path('create/', post_create, name='post-create'),
-    path('post/<id>/', post, name='post-detail'),
-    #path('post/<pk>/', PostDetailView.as_view(), name='post-detail'),
+    #path('create/', post_create, name='post-create'),
+    path('create/', PostCreateView.as_view(), name='post-create'),
 
-    path('post/<id>/update/', post_update, name='post-update'),
-    path('post/<id>/delete/', post_delete, name='post-delete'),
+    #path('post/<id>/', post, name='post-detail'),
+    path('post/<pk>/', PostDetailView.as_view(), name='post-detail'),
+
+    #path('post/<pk>/update/', post_update, name='post-update'),
+    path('post/<pk>/update/', PostUpdateView.as_view(), name='post-update'),
+
+    #path('post/<pk>/delete/', post_delete, name='post-delete'),
+    path('post/<pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('accounts/', include('allauth.urls'))
 ]
